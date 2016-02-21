e47103035608b7c47218471fc8d29d37eff9b59f
nghia2080 <kidzen@gmail.com>
2016-02-21 12:35:06 +0700

    s

--- bot2.sh
+++ bot2.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 while true
 do
-DISPLAY=:1 google-chrome --no-sandbox --new-window --user-data-dir="/root/chromeBotTE" --disable-popup-blocking --incognito "http://247webhits.com/exchange?key=gxjepc7sqbgzshrvdj7j6otp4xdi711sz8xntashuwd0thq3trm7bqfwnpo3wvjz" & sleep 1000
-pkill -f -9 google-chrome
-pkill -f -9 google-chrome
+DISPLAY=:1 chromium --no-sandbox --new-window  --disable-popup-blocking --incognito "http://247webhits.com/exchange?key=gxjepc7sqbgzshrvdj7j6otp4xdi711sz8xntashuwd0thq3trm7bqfwnpo3wvjz" & sleep 1000
+pkill -f -9 chromium
 sleep 5
 done
