b2e009b1f8743c2acab3054eaa1a5a80dcf86271
nghia2080 <kidzen@gmail.com>
2016-02-28 11:34:08 +0700

    cm

--- bot2.sh
+++ bot2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-nohup DISPLAY=:1 chromium --no-sandbox --new-window  --disable-popup-blocking --incognito "http://247webhits.com/exchange?key=gxjepc7sqbgzshrvdj7j6otp4xdi711sz8xntashuwd0thq3trm7bqfwnpo3wvjz" "http://exchange.hornyte.com/mE1qwiLxd7tl9dPYWlp5k6w74U03ZDAy" & sleep 1000
+DISPLAY=:1 chromium --no-sandbox --new-window  --disable-popup-blocking --incognito "http://247webhits.com/exchange?key=gxjepc7sqbgzshrvdj7j6otp4xdi711sz8xntashuwd0thq3trm7bqfwnpo3wvjz" "http://exchange.hornyte.com/mE1qwiLxd7tl9dPYWlp5k6w74U03ZDAy" & sleep 1000
 pkill -f -9 chromium
 sleep 5
 done
